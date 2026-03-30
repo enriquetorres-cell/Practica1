12235e6c32d8aaea4fedf2cb31dca8ea8149e43f
Denilson <denilson.cuadros@utec.edu.pe>
2026-03-30 17:23:28 -0500

    cambios gaaa

--- enrique.py
+++ enrique.py
@@ -1,4 +1,5 @@
 Ojala que se vea el texto
 
 Esta es la linea de la actividad que dejo Enrique ....
-
+def sumartoria_de_algun_numero (numero_uno, numero_dos):
+    return numero_uno+numero_dos fafbhjkdmnbhgsdfnkmdasnghefndkm vbhbj
